350137c0ef5ef7ab9dcedcde16db0cd5d389b4b6
John Ford <jford11@hawk.iit.edu>
2023-02-17 17:26:10 -0600

    Update secure-splunk.sh

--- Scripts/secure-splunk.sh
+++ Scripts/secure-splunk.sh
@@ -4,9 +4,10 @@
 service sshd stop
 rm /etc/init.d/sshd
 
-# Fix firewall rules
+# Remove firewall rules
 iptables -F
 
+# Only allow incoming traffic on tcp/8000, tcp/8189, tcp/9997, udp/514, ping
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp --dport 8000 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8189 -j ACCEPT
@@ -16,6 +17,7 @@ iptables -A INPUT -p icmp --icmp-type 8 -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -P INPUT DROP
 
+# Block all outgoing traffic except for established connections
 iptables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 iptables -P OUTPUT DROP
